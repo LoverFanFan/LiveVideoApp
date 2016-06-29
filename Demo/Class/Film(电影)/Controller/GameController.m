f15f3bf121453435bf22ced1614aabe1707acfc9
吴启凡 <827500994@qq.com>
2016-06-29 18:01:19 +0800

    test test

--- Demo/Class/Film(电影)/Controller/GameController.m
+++ Demo/Class/Film(电影)/Controller/GameController.m
@@ -52,6 +52,10 @@
 #pragma mark - 初始化各个view
 -(void)setUpViews{
     NSLog(@"这是小彬彬");
+    NSLog(@"这是小彬彬");
+    NSLog(@"这是小彬彬");
+    NSLog(@"这是小彬彬");
+    NSLog(@"这是小彬彬");
 
     UICollectionViewFlowLayout *flowLayout= [[UICollectionViewFlowLayout alloc]init];
     
